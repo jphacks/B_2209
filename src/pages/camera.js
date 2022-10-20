465fcf84bfcfdc17603a09b43fe9bd2e12fe7799
Gyuchan3 <ushimaru.chiaki39@gmail.com>
2022-10-21 01:04:19 +0900

    :sparkles: feat: camera.jsに外部リンクに遷移するボタンを追加
    仮としてgoogle.comに遷移するボタンを配置した。
    issue #9

--- src/pages/camera.js
+++ src/pages/camera.js
@@ -1,12 +1,47 @@
 import { Header } from '../components/Header';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { teal } from '@mui/material/colors';
+import { styled } from '@mui/material/styles';
+import { css } from '@emotion/react';
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(teal[500]),
+  backgroundColor: teal[500],
+  '&:hover': {
+    backgroundColor: teal[700],
+  },
+}));
 
 const CameraPage = () => {
   return (
     <>
       <Header>ARマーカーの読み取り</Header>
-      <h1>This is camera page!</h1>
+      <Container component="main" maxWidth="xs" css={cssContainer}>
+        <ColorButton
+          variant="contained"
+          target="_blank"
+          rel="noopener"
+          href="https://google.com" // TODO: 実際のARマーカー読み取り画面のURLに変更
+          size="large"
+          css={cssButton}
+          color="secondary"
+        >
+          読み取り画面を開く
+        </ColorButton>
+      </Container>
     </>
   );
 };
+
+const cssButton = css`
+  margin-top: 64px;
+`;
+
+const cssContainer = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default CameraPage;
