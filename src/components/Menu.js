0b247bae562153f688d88ff01250fb4a74e0da00
Gyuchan3 <ushimaru.chiaki39@gmail.com>
2022-10-20 14:59:03 +0900

    :sparkles: feat: メニューに影をつけた
    メニューと本文の境界がわかるように影をつけた

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Paper from '@mui/material/Paper';
 import ListIcon from '@mui/icons-material/List';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import QrCode2Icon from '@mui/icons-material/QrCode2';
 import EditIcon from '@mui/icons-material/Edit';
-import { css } from '@emotion/react';
 import { useRouter } from 'next/router';
 
 export const Menu = () => {
@@ -20,41 +20,39 @@ export const Menu = () => {
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
-    // setTimeout(() => {
-      router.push(`${newValue}`);
-    // }, 0);
+    router.push(`${newValue}`);
   };
 
   return (
-    <BottomNavigation
-      showLabels
-      css={cssBottomNavigation}
-      value={value}
-      onChange={handleChange}
-    >
-      <BottomNavigationAction
-        label="友達一覧"
-        value="/"
-        icon={<ListIcon />}
-      />
-      <BottomNavigationAction
-        label="読み取り"
-        value="camera"
-        icon={<PhotoCameraIcon />}
-      />
-      <BottomNavigationAction
-        label="マーカー"
-        value="ARmarker"
-        icon={<QrCode2Icon />}
-      />
-      <BottomNavigationAction label="編集" value="edit" icon={<EditIcon />} />
-    </BottomNavigation>
+    <>
+      <Paper
+        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, width: '100%' }}
+        elevation={3}
+        component="nav"
+      >
+        <BottomNavigation showLabels value={value} onChange={handleChange}>
+          <BottomNavigationAction
+            label="友達一覧"
+            value="/"
+            icon={<ListIcon />}
+          />
+          <BottomNavigationAction
+            label="読み取り"
+            value="camera"
+            icon={<PhotoCameraIcon />}
+          />
+          <BottomNavigationAction
+            label="マーカー"
+            value="ARmarker"
+            icon={<QrCode2Icon />}
+          />
+          <BottomNavigationAction
+            label="編集"
+            value="edit"
+            icon={<EditIcon />}
+          />
+        </BottomNavigation>
+      </Paper>
+    </>
   );
 };
-
-const cssBottomNavigation = css`
-  width: 100%;
-  margin: 0 auto;
-  position: fixed;
-  bottom: 0px;
-`;
