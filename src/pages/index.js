f73aa1b9b90c2d995a9b535bff917ff405619efc
Gyuchan3 <ushimaru.chiaki39@gmail.com>
2022-10-22 00:54:34 +0900

    :sparkles: feat: データベースの内容を表示
    データベースに登録されたユーザーのaccount（氏名）とicon（一言）をリスト表示するようにしました。
    issue #8

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,59 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { Header } from '../components/Header';
 import HEAD from 'next/head';
 import Script from 'next/script';
-
 import Container from '@mui/material/Container';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import { css } from '@emotion/react';
-// import prisma from '../../lib/prisma';
-const makeSerializable = (o) => {
-  return JSON.parse(JSON.stringify(o));
-};
 
 const IndexPage = (props) => {
-  async function get_ar() {
-    try {
-      const result = await fetch(`/api/get`, {
+  // async function get_ar() {
+  //   try {
+  //     const result = await fetch(`/api/get`, {
+  //       method: 'GET',
+  //     });
+  //     const data = await result.json();
+  //     console.log('data');
+  //     console.log(data); //dataの中身を見てください(ブラウザのdeveloperのconsoleに表示)
+  //     return data;
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch('/api/get', {
         method: 'GET',
       });
-      const data = await result.json();
-      // console.log(data); //dataの中身を見てください(ブラウザのdeveloperのconsoleに表示)
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  // const dbList = get_ar();
-  // const thenPromise = get_ar().then(value => {
-  //   // console.log(value);
-  //   return value;
-  // });
-  console.log(get_ar());
-  // // TODO: 実際のデータベースに置き換える
-  const dbList = [
-    { id: 3, category: 'text', content: 'nice to meet you', authorId: 3 },
-    { id: 4, category: 'image', content: 'soccer.jpg', authorId: 3 },
-  ];
-  const db = get_ar();
-  console.log(typeof db);
+      const data = await response.json();
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
+  // // 使っていないダミー
+  // const dbList = [
+  //   {
+  //     id: 1,
+  //     key: 1,
+  //     account: 'あいうえお',
+  //     name: 'hoge',
+  //     icon: 'よろしくお願いします！',
+  //   },
+  //   {
+  //     id: 2,
+  //     key: 2,
+  //     account: 'かきくけこ',
+  //     name: 'hoge',
+  //     icon: 'やっほー！趣味は読書だよ',
+  //   },
+  // ];
+
   const FriendListItem = ({ account, text }) => (
     <ListItem divider>
       <ListItemText primary={account} secondary={text} />
@@ -48,8 +63,8 @@ const IndexPage = (props) => {
   const FriendList = () => (
     <>
       <List component="div" sx={{ width: '100%' }}>
-        {dbList.map((content) => (
-          <FriendListItem account={content.category} text={content.content} />
+        {users.map((user) => (
+          <FriendListItem account={user.account} text={user.icon} />
         ))}
       </List>
     </>
@@ -84,7 +99,7 @@ const IndexPage = (props) => {
           src="threex/threex-armarkercontrols.js"
         ></Script>
       </HEAD>
-      <Header>友達一覧</Header>
+      <Header>登録者一覧</Header>
       <Container component="main" maxWidth="xs" css={cssContainer}>
         <FriendList />
       </Container>
@@ -100,15 +115,3 @@ const cssContainer = css`
 `;
 
 export default IndexPage;
-/*
-//登録番号3番の人のarの文字列と画像ファイルを取ってくる
-//browser上でprismaはうごかないよって怒られる
-async function get_ar_num() {
-  const ar_num = await prisma.post.findMany({
-    where: { authorId: 3 },
-  });
-  console.log(ar_num);
-}
-
-get_ar_num();
-*/
