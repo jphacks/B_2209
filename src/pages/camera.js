9e882503eef65c4f89ee4efbf864e095e046a270
haruponponpopon <kobayashi-haruka784@g.ecc.u-tokyo.ac.jp>
2022-10-21 11:23:02 +0900

    :sparkles: feat:      arのidが入力された時に、userデータを入手

--- src/pages/camera.js
+++ src/pages/camera.js
@@ -1,6 +1,19 @@
 import { Header } from '../components/Header';
 
 const CameraPage = () => {
+  async function get_ar() {
+    try {
+      const id = 1; //今はidが1の人のデータを取得するようにコードを書いています。ここの数字を変えてください。
+      const result = await fetch(`/api/get/${id}`, {
+        method: 'GET',
+      });
+      const data = await result.json();
+      console.log(data); //dataの中身を見てください(ブラウザのdeveloperのconsoleに表示)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  get_ar();
   return (
     <>
       <Header>ARマーカーの読み取り</Header>
