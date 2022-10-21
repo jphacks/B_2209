3eb8e0548511eb880f03f5d0a9ba27495497ab1a
Gyuchan3 <ushimaru.chiaki39@gmail.com>
2022-10-21 19:37:29 +0900

    :lipstick: style: リストの表示を画面全体の中央寄せにした

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,15 @@ import { Header } from '../components/Header';
 import HEAD from 'next/head';
 import Script from 'next/script';
 
+import Container from '@mui/material/Container';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import { css } from '@emotion/react';
 // import prisma from '../../lib/prisma';
-
-// TODO: 実際のデータベースに置き換える
-const dbList = [
-  { id: 3, category: 'text', content: 'nice to meet you', authorId: 3 },
-  { id: 4, category: 'image', content: 'soccer.jpg', authorId: 3 },
-];
-
-const FriendListItem = ({ account, text }) => (
-  <ListItem divider>
-    <ListItemText primary={account} secondary={text} />
-  </ListItem>
-);
-
-const FriendList = () => (
-  <>
-    <List component="div" sx={{ width: '100%', maxWidth: 360 }}>
-      {dbList.map((content) => (
-        <FriendListItem account={content.category} text={content.content} />
-      ))}
-    </List>
-  </>
-);
+const makeSerializable = (o) => {
+  return JSON.parse(JSON.stringify(o));
+};
 
 const IndexPage = (props) => {
   async function get_ar() {
@@ -37,12 +20,41 @@ const IndexPage = (props) => {
         method: 'GET',
       });
       const data = await result.json();
-      console.log(data); //dataの中身を見てください(ブラウザのdeveloperのconsoleに表示)
+      // console.log(data); //dataの中身を見てください(ブラウザのdeveloperのconsoleに表示)
+      return data;
     } catch (error) {
       console.error(error);
     }
   }
-  get_ar();
+  // const dbList = get_ar();
+  // const thenPromise = get_ar().then(value => {
+  //   // console.log(value);
+  //   return value;
+  // });
+  console.log(get_ar());
+  // // TODO: 実際のデータベースに置き換える
+  const dbList = [
+    { id: 3, category: 'text', content: 'nice to meet you', authorId: 3 },
+    { id: 4, category: 'image', content: 'soccer.jpg', authorId: 3 },
+  ];
+  const db = get_ar();
+  console.log(typeof db);
+  const FriendListItem = ({ account, text }) => (
+    <ListItem divider>
+      <ListItemText primary={account} secondary={text} />
+    </ListItem>
+  );
+
+  const FriendList = () => (
+    <>
+      <List component="div" sx={{ width: '100%' }}>
+        {dbList.map((content) => (
+          <FriendListItem account={content.category} text={content.content} />
+        ))}
+      </List>
+    </>
+  );
+
   return (
     <div>
       <HEAD>
@@ -73,217 +85,19 @@ const IndexPage = (props) => {
         ></Script>
       </HEAD>
       <Header>友達一覧</Header>
-      <FriendList />
+      <Container component="main" maxWidth="xs" css={cssContainer}>
+        <FriendList />
+      </Container>
     </div>
   );
-
-  // var scene, camera, renderer, clock, deltaTime, totalTime;
-
-  // var arToolkitSource, arToolkitContext;
-
-  // var markerRoot1, markerRoot2;
-
-  // var mesh1;
-
-  // //export let readId = -1;
-
-  // initialize();
-  // animate();
-
-  // function initialize() {
-  //   scene = new THREE.Scene();
-
-  //   let ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
-  //   scene.add(ambientLight);
-
-  //   camera = new THREE.Camera();
-  //   scene.add(camera);
-
-  //   renderer = new THREE.WebGLRenderer({
-  //     antialias: true,
-  //     alpha: true,
-  //   });
-  //   renderer.setClearColor(new THREE.Color('lightgrey'), 0);
-  //   renderer.setSize(640, 480);
-  //   renderer.domElement.style.position = 'absolute';
-  //   renderer.domElement.style.top = '0px';
-  //   renderer.domElement.style.left = '0px';
-  //   document.body.appendChild(renderer.domElement);
-
-  //   clock = new THREE.Clock();
-  //   deltaTime = 0;
-  //   totalTime = 0;
-
-  //   ////////////////////////////////////////////////////////////
-  //   // setup arToolkitSource
-  //   ////////////////////////////////////////////////////////////
-
-  //   arToolkitSource = new THREEx.ArToolkitSource({
-  //     sourceType: 'webcam',
-  //   });
-
-  //   function onResize() {
-  //     arToolkitSource.onResize();
-  //     arToolkitSource.copySizeTo(renderer.domElement);
-  //     if (arToolkitContext.arController !== null) {
-  //       arToolkitSource.copySizeTo(arToolkitContext.arController.canvas);
-  //     }
-  //   }
-
-  //   arToolkitSource.init(function onReady() {
-  //     onResize();
-  //   });
-
-  //   // handle resize event
-  //   window.addEventListener('resize', function () {
-  //     onResize();
-  //   });
-
-  //   ////////////////////////////////////////////////////////////
-  //   // setup arToolkitContext
-  //   ////////////////////////////////////////////////////////////
-
-  //   // create atToolkitContext
-  //   arToolkitContext = new THREEx.ArToolkitContext({
-  //     cameraParametersUrl: 'data/camera_para.dat',
-  //     detectionMode: 'mono',
-  //   });
-
-  //   // copy projection matrix to camera when initialization complete
-  //   arToolkitContext.init(function onCompleted() {
-  //     camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
-  //   });
-
-  //   ////////////////////////////////////////////////////////////
-  //   // setup markerRoots
-  //   ////////////////////////////////////////////////////////////
-
-  //   let loader = new THREE.TextureLoader();
-  //   let texture = loader.load('images/border.png');
-
-  //   let patternArray = [
-  //     'letterA',
-  //     'letterB',
-  //     'letterC',
-  //     'letterD',
-  //     'letterF',
-  //     'kanji',
-  //     'hiro',
-  //   ];
-  //   let colorArray = [
-  //     0xff0000, 0xff8800, 0xffff00, 0x00cc00, 0x0000ff, 0xcc00ff, 0xcccccc,
-  //   ];
-  //   //markerControlsを配列の要素として取り扱う
-  //   let markerControlsarray = new Array();
-  //   let readId = -1;
-
-  //   for (let i = 0; i < 7; i++) {
-  //     let markerRoot = new THREE.Group();
-  //     scene.add(markerRoot);
-  //     //let markerControls = new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
-  //     //	type : 'pattern', patternUrl : "data/" + patternArray[i] + ".patt",
-  //     //});
-  //     markerControlsarray.push(
-  //       new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
-  //         type: 'pattern',
-  //         patternUrl: 'data/' + patternArray[i] + '.patt',
-  //       })
-  //     );
-
-  //     markerControlsarray[markerControlsarray.length - 1].addEventListener(
-  //       'markerFound',
-  //       () => {
-  //         readId = i + 1;
-  //         console.log('marker' + readId + ' is visible');
-  //       }
-  //     );
-
-  //     let mesh = new THREE.Mesh(
-  //       new THREE.CubeGeometry(1.25, 1.25, 1.25),
-  //       new THREE.MeshBasicMaterial({
-  //         color: colorArray[i],
-  //         map: texture,
-  //         transparent: true,
-  //         opacity: 0.5,
-  //       })
-  //     );
-  //     mesh.position.y = 1.25 / 2;
-  //     markerRoot.add(mesh);
-  //   }
-
-  //   //構造の予定：
-  //   //全部のマーカーのmarkercontrolsを動かす
-  //   //表示されたマーカーからそれに対応するARを表示して、配列番号をidとする
-  //   //idからデータベースを用いて文章を取り込む
-
-  //   //１つめ
-  //   //let markerRoot0 = new THREE.Group();//ここはfor文内で同じものを用いても大丈夫
-  //   //scene.add(markerRoot0);
-  //   //let markerControls0 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot0, {
-  //   //	type : 'pattern', patternUrl : "data/" + patternArray[0] + ".patt",
-  //   //});
-  //   //markerControlsarray.push(new THREEx.ArMarkerControls(arToolkitContext, markerRoot0, {
-  //   //	type : 'pattern', patternUrl : "data/" + patternArray[0] + ".patt",
-  //   //}));
-
-  //   //markerControlsarray[markerControlsarray.length - 1].addEventListener("markerFound", () => {
-  //   // マーカーが見つかっている時は毎秒呼ばれる
-  //   //うまくいかなかった、、for文が一瞬だからすぐ捨てられるのが原因？
-  //   //	console.log("marker0 is visible");
-  //   //});
-
-  //   /*
-  //       let mesh0 = new THREE.Mesh(
-  //         new THREE.CubeGeometry(1.25,1.25,1.25),
-  //         new THREE.MeshBasicMaterial({color:colorArray[0], map:texture, transparent:true, opacity:0.5})
-  //       );
-
-  //       mesh0.position.y = 1.25/2;
-  //       //作った正方形を表示するものリストの追加
-  //       markerRoot0.add( mesh0 );
-
-  //       //二つ目
-  //       let markerRoot1 = new THREE.Group();
-  //       scene.add(markerRoot1);
-  //       let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
-  //         type : 'pattern', patternUrl : "data/" + patternArray[1] + ".patt",
-  //       });
-
-  //       markerControls1.addEventListener("markerFound", () => {
-  //         // マーカーが見つかっている時は毎秒呼ばれる
-  //         //うまくいかなかった、、for文が一瞬だからすぐ捨てられるのが原因？
-  //         console.log("marker1 is visible");
-  //       });
-
-  //       let mesh1 = new THREE.Mesh(
-  //         new THREE.CubeGeometry(1.25,1.25,1.25),
-  //         new THREE.MeshBasicMaterial({color:colorArray[1], map:texture, transparent:true, opacity:0.5})
-  //       );
-
-  //       mesh1.position.y = 1.25/2;
-  //       //作った正方形を表示するものリストの追加
-  //       markerRoot0.add( mesh1 );
-  //       */
-  // }
-
-  // function update() {
-  //   // update artoolkit on every frame
-  //   if (arToolkitSource.ready !== false)
-  //     arToolkitContext.update(arToolkitSource.domElement);
-  // }
-
-  // function render() {
-  //   renderer.render(scene, camera);
-  // }
-
-  // function animate() {
-  //   requestAnimationFrame(animate);
-  //   deltaTime = clock.getDelta();
-  //   totalTime += deltaTime;
-  //   update();
-  //   render();
-  // }
 };
+
+const cssContainer = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // flex-wrap: wrap;
+`;
 
 export default IndexPage;
 /*
@@ -298,72 +112,3 @@ async function get_ar_num() {
 
 get_ar_num();
 */
-
-// import Head from 'next/head';
-// import Image from 'next/image';
-// import styles from '../styles/Home.module.css';
-// import { css } from '@emotion/react';
-
-// export default function Home() {
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>Create Next App</title>
-//         <meta name="description" content="Generated by create next app" />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <main className={styles.main}>
-//         <h1 className={styles.title}>Welcome to フォロー画面!</h1>
-
-//         <p className={styles.description}>
-//           Get started by editing{' '}
-//           <code className={styles.code}>pages/index.js</code>
-//         </p>
-
-//         <div className={styles.grid}>
-//           <a href="https://nextjs.org/docs" className={styles.card}>
-//             <h2>Documentation &rarr;</h2>
-//             <p>Find in-depth information about Next.js features and API.</p>
-//           </a>
-
-//           <a href="https://nextjs.org/learn" className={styles.card}>
-//             <h2>Learn &rarr;</h2>
-//             <p>Learn about Next.js in an interactive course with quizzes!</p>
-//           </a>
-
-//           <a
-//             href="https://github.com/vercel/next.js/tree/canary/examples"
-//             className={styles.card}
-//           >
-//             <h2>Examples &rarr;</h2>
-//             <p>Discover and deploy boilerplate example Next.js projects.</p>
-//           </a>
-
-//           <a
-//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-//             className={styles.card}
-//           >
-//             <h2>Deploy &rarr;</h2>
-//             <p>
-//               Instantly deploy your Next.js site to a public URL with Vercel.
-//             </p>
-//           </a>
-//         </div>
-//       </main>
-
-//       <footer className={styles.footer}>
-//         <a
-//           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Powered by{' '}
-//           <span className={styles.logo}>
-//             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-//           </span>
-//         </a>
-//       </footer>
-//     </div>
-//   );
-// }
