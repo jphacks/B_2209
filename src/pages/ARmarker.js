53eed46a537447837b319f20ee73585f1de19180
Gyuchan3 <ushimaru.chiaki39@gmail.com>
2022-10-24 01:37:39 +0900

    :sparkles: feat: ユーザーに対応したマーカー画像を表示
    マーカー画面で登録済みのユーザーのリストが表示される。
    表示したいユーザーをクリックするとARマーカー画像が表示される。
    その画像を読み取ると、登録したテキストと画像がAR表示される。

--- src/pages/ARmarker.js
+++ src/pages/ARmarker.js
@@ -1,22 +1,102 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { Header } from '../components/Header';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import List from '@mui/material/List';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Paper from '@mui/material/Paper';
 import { teal } from '@mui/material/colors';
 import { styled } from '@mui/material/styles';
 import { css } from '@emotion/react';
 
 const ARMarkerPage = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch('/api/user/get', {
+        method: 'GET',
+      });
+      const data = await response.json();
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
+  const MarkerButtonList = () => (
+    <>
+      <List component="div" sx={{ width: '100%' }}>
+        {users.map((user) => (
+          <MarkerButtonListItem
+            id={user.id}
+            account={user.account}
+            text={user.text}
+            icon={user.icon}
+            key={user.id}
+          />
+        ))}
+      </List>
+    </>
+  );
+
+  const MarkerButtonListItem = ({ id, account, text, icon }) => {
+    const image = get_ARMarker(id);
+    return (
+      <Paper>
+        <ListItemButton
+          key={id}
+          alignItems="flex-start"
+          target="_blank"
+          rel="noopener"
+          href={image}
+          css={cssListItemButton}
+        >
+          <ListItemAvatar>
+            <Avatar alt={account} src={icon} />
+          </ListItemAvatar>
+          <ListItemText primary={account} secondary={text} />
+        </ListItemButton>
+      </Paper>
+    );
+  };
+
   return (
     <>
       <Header>ARマーカーの表示</Header>
-      <p css={cssP}>ユーザーを選んでください</p>
+      <p css={cssP}>ユーザーを選択してください</p>
       <Container component="main" maxWidth="xs" css={cssContainer}>
         {markerList.map((content) => (
-          <LinkButton user={content.user} image={content.image} />
+          <LinkButton
+            user={content.user}
+            image={content.image}
+            key={content.key}
+          />
         ))}
+        <MarkerButtonList />
       </Container>
     </>
   );
+};
+
+const get_ARMarker = (id) => {
+  // public/myfile.jsと同じ配列
+  // TODO:
+  // idとARマーカー画像の対応をデータベースで管理
+  // もしくはARマーカー画像を固定にし、ARマーカーの画像ファイル名をmarker[id].png等に変更
+  const patternArray = [
+    'letterA',
+    'letterB',
+    'letterC',
+    'letterD',
+    'letterF',
+    'kanji',
+    'hiro',
+  ];
+
+  return `ARmarker/${patternArray[id - 1]}.png`;
 };
 
 const ColorButton = styled(Button)(({ theme }) => ({
@@ -76,6 +156,10 @@ const markerList = [
     image: '/ARmarker/hiro.png',
   },
 ];
+
+const cssListItemButton = css`
+  margin: 8px 0;
+`;
 
 const cssButton = css`
   margin: 16px;
